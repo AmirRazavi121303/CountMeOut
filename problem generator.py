09121f13087e130638b357f4bcc852c8dea83a20
AmirRazavi121303 <amirsh.razavi@gmail.com>
2025-06-09 22:20:58 +0200

    Add files via upload

--- problem generator.py
+++ problem generator.py
@@ -1,19 +1,23 @@
-
+import pandas as pd 
 import random
 
-tries = int(input("How many problems do you want? "))
+tries = 5 #int(input("How many problems do you want? "))
 
-problems = ""
 n = tries - (tries-1)
 
-def generate_random_problem(tries, problems, n):
+def generate_random_problem(tries, n):
+    problems = []
     while tries > 0: 
         a, b, c = random.randint(1,9), random.randint(1,9), random.randint(1,9)
         ops = ['+', '-', '*', '/']
         op1, op2 = random.choice(ops[0:3]), random.choice(ops) #just cuz i dont ever want the first operator to be a division 
-        problems += f"Problem {n}: {a} {op1} {b} {op2} {c} \n" 
+        problems.append(f"Problem {n}: {a} {op1} {b} {op2} {c} ")
         tries -= 1;
         n += 1
     return problems
 
-print(generate_random_problem(tries, problems, n))
+problems = generate_random_problem(tries, n)
+columns = [f"Problem {i+1}" for i in range(len(problems))]
+df = pd.DataFrame(problems, columns=["Problem"])
+
+print(df)
